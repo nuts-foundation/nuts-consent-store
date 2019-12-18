1bfce6b1c99377d7e92b7b293c4951f999572d7c
stevenvegt <steven.vandervegt@nedap.com>
2019-12-18 14:43:41 +0100

    Fix down migration
    Add default date on NULL columns.
    Split data copy step into 2 queries, with and without end_date,

--- migrations/4_alter_consent_record_make_valid_to_optional.down.sql
+++ migrations/4_alter_consent_record_make_valid_to_optional.down.sql
@@ -6,7 +6,7 @@ CREATE TABLE consent_record (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     patient_consent_id VARCHAR(255) REFERENCES patient_consent(id),
     valid_from DATE NOT NULL,
-    valid_to DATE NOT NULL,
+    valid_to DATE NOT NULL DEFAULT "2100-01-01",
     hash VARCHAR(255) NOT NULL UNIQUE,
     version INTEGER DEFAULT 1,
     uuid VARCHAR(255),
@@ -15,6 +15,7 @@ CREATE TABLE consent_record (
 
 CREATE UNIQUE INDEX uniq_record_version ON consent_record(patient_consent_id, uuid, version);
 
-INSERT INTO consent_record SELECT id, patient_consent_id, valid_from, valid_to, hash, version, uuid, previous_hash FROM consent_record_tmp;
+INSERT INTO consent_record SELECT id, patient_consent_id, valid_from, valid_to, hash, version, uuid, previous_hash FROM consent_record_tmp WHERE valid_to IS NOT NULL;
+INSERT INTO consent_record(id, patient_consent_id, valid_from, hash, version, uuid, previous_hash) SELECT id, patient_consent_id, valid_from, hash, version, uuid, previous_hash FROM consent_record_tmp WHERE valid_to IS NULL;
 
 DROP TABLE consent_record_tmp;
